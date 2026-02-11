0f2cd631efb4630ec4853d83745b4b63de00fd17
Gokul S <sggokul762@gmail.com>
2026-02-11 20:11:22 +0000

    Webhook demo

--- src/main/java/com/example/devz_resale/controller/PageController.java
+++ src/main/java/com/example/devz_resale/controller/PageController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    
     @GetMapping("/")
     public String index() {
         return "forward:/index.html";
